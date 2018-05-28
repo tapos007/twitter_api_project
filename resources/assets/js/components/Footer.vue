2a5082a2089763288c7811f81f142f63b53d0a55
Khandaker Akash <akash.cse10@gmail.com>
2018-05-29 03:46:58 +0600

    Vue setup in this project and component creation done, you can use for test it by: npm run dev

---
+++ resources/assets/js/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="footer-panel">
+                            <div class="foot-txt">
+                                <p>&copy;Copyright 2018. All Rights Reserved.</p>
+                            </div>
+                            <ul class="social-nav d-inline">
+                                <li class="facebook">
+                                    <a href="#"><i class="fa fa-facebook-official"></i></a>
+                                </li>
+                                <li class="instagram">
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                </li>
+                                <li class="twitter">
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li class="youtube">
+                                    <a href="#"><i class="fa fa-youtube-play"></i></a>
+                                </li>
+                            </ul>
+                            <ul class="foot-nav d-inline">
+                                <li><a href="#">Trust and Safety</a></li>
+                                <li><a href="#">Terms of Use</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/Profile.vue
@@ -0,0 +1,177 @@
+<template>
+    <div>
+        <div class="panel-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel-header">
+                            <div class="search-box">
+                                <form action="#" class="form-search">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Search">
+                                    </div>
+                                </form>
+                            </div>
+
+                            <div class="sort-box">
+                                <div class="select-wrapper">
+                                    <select name="sort">
+                                        <option>Sort By</option>
+                                        <option value="popularity">Popularity</option>
+                                        <option value="price low to high">Price Low to High</option>
+                                        <option value="price high to low">Price High to Low</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="card-wrapper">
+                            <div class="card text-center">
+                                <div class="card-cover"></div>
+                                <div class="thumb">
+                                    <img src="{{ asset('images/thumb.jpg') }}" alt="user">
+                                </div>
+                                <h3>Lonut John</h3>
+                                <p>Interaction Designer</p>
+                                <span class="amount">$ 570</span>
+                                <div class="divider"></div>
+                                <div class="card-tbl d-table">
+                                    <div class="card-col text-left">
+                                        <span>$1,798,336</span>
+                                        <span>Market Cap</span>
+                                    </div>
+                                    <div class="card-col text-right">
+                                        <span>138%</span>
+                                        <span>Funded</span>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-light">Buy Now</a>
+                            </div> <!-- card -->
+                            <div class="card text-center">
+                                <div class="card-cover"></div>
+                                <div class="thumb">
+                                    <img src="{{ asset('images/thumb.jpg') }}" alt="user">
+                                </div>
+                                <h3>Lonut John</h3>
+                                <p>Interaction Designer</p>
+                                <span class="amount">$ 570</span>
+                                <div class="divider"></div>
+                                <div class="card-tbl d-table">
+                                    <div class="card-col text-left">
+                                        <span>$1,798,336</span>
+                                        <span>Market Cap</span>
+                                    </div>
+                                    <div class="card-col text-right">
+                                        <span>138%</span>
+                                        <span>Funded</span>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-light">Buy Now</a>
+                            </div> <!-- card -->
+                            <div class="card text-center">
+                                <div class="card-cover"></div>
+                                <div class="thumb">
+                                    <img src="{{ asset('images/thumb.jpg') }}" alt="user">
+                                </div>
+                                <h3>Lonut John</h3>
+                                <p>Interaction Designer</p>
+                                <span class="amount">$ 570</span>
+                                <div class="divider"></div>
+                                <div class="card-tbl d-table">
+                                    <div class="card-col text-left">
+                                        <span>$1,798,336</span>
+                                        <span>Market Cap</span>
+                                    </div>
+                                    <div class="card-col text-right">
+                                        <span>138%</span>
+                                        <span>Funded</span>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-light">Buy Now</a>
+                            </div> <!-- card -->
+                            <div class="card text-center">
+                                <div class="card-cover"></div>
+                                <div class="thumb">
+                                    <img src="{{ asset('images/thumb.jpg') }}" alt="user">
+                                </div>
+                                <h3>Lonut John</h3>
+                                <p>Interaction Designer</p>
+                                <span class="amount">$ 570</span>
+                                <div class="divider"></div>
+                                <div class="card-tbl d-table">
+                                    <div class="card-col text-left">
+                                        <span>$1,798,336</span>
+                                        <span>Market Cap</span>
+                                    </div>
+                                    <div class="card-col text-right">
+                                        <span>138%</span>
+                                        <span>Funded</span>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-light">Buy Now</a>
+                            </div>
+
+                            <div class="card text-center">
+                                <div class="card-cover"></div>
+                                <div class="thumb">
+                                    <img src="{{ asset('images/thumb.jpg') }}" alt="user">
+                                </div>
+                                <h3>Lonut John</h3>
+                                <p>Interaction Designer</p>
+                                <span class="amount">$ 570</span>
+                                <div class="divider"></div>
+                                <div class="card-tbl d-table">
+                                    <div class="card-col text-left">
+                                        <span>$1,798,336</span>
+                                        <span>Market Cap</span>
+                                    </div>
+                                    <div class="card-col text-right">
+                                        <span>138%</span>
+                                        <span>Funded</span>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-light">Buy Now</a>
+                            </div> <!-- card -->
+                            <div class="card text-center">
+                                <div class="card-cover"></div>
+                                <div class="thumb">
+                                    <img src="{{ asset('images/thumb.jpg') }}" alt="user">
+                                </div>
+                                <h3>Lonut John</h3>
+                                <p>Interaction Designer</p>
+                                <span class="amount">$ 570</span>
+                                <div class="divider"></div>
+                                <div class="card-tbl d-table">
+                                    <div class="card-col text-left">
+                                        <span>$1,798,336</span>
+                                        <span>Market Cap</span>
+                                    </div>
+                                    <div class="card-col text-right">
+                                        <span>138%</span>
+                                        <span>Funded</span>
+                                    </div>
+                                </div>
+                                <a href="#" class="btn btn-light">Buy Now</a>
+                            </div>
+
+                            <div class="loader text-center">
+                                <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Profile"
+    }
+</script>
+
+<style scoped>
+
+</style>
